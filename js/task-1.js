1375fb27e680193e9ece57f90cd179bc2263065b
Alexandr <alexandr.kopyletz@gmail.com>
2024-06-04 22:29:33 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,9 @@
 const items = document.querySelectorAll(".item");
 console.log(`Number of categories: ${items.length}`);
+items.forEach(category => {
+    const categoryTitle = category.querySelector('h2').textContent;
+    const categoryItems = category.querySelectorAll('li').length;
+    console.log(`Category: ${categoryTitle}`);
+    console.log(`Elements: ${categoryItems}`);
+  });
+  
