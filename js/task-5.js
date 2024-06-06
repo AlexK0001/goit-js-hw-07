7aaa9a79e78317c20d22de90158469b6c09a57eb
Alexandr <alexandr.kopyletz@gmail.com>
2024-06-06 23:43:51 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const  colorNext = document.querySelector(".color");
+
+const handleClick = () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorNext.textContent = newColor;
+}
+
+changeColor.addEventListener("click", handleClick);
